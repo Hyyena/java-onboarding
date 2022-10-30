1be9a11d551e159895764c10752cdd54d931e597
Hyyena <sjtxm0320@gmail.com>
2022-10-30 18:32:46 +0900

    :sparkles: Add odd check method for exception handling

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,12 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static boolean isLeftPageOdd(List<Integer> book) {
+        int leftPage = book.get(0);
+
+        return leftPage % 2 == 1;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
