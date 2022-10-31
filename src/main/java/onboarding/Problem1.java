40e54f1213da388a8a78a9320d7cefb69c990c2a
Hyyena <sjtxm0320@gmail.com>
2022-10-31 22:39:51 +0900

    :recycle: Refactor: Refactor return scores to list method to determine scores method

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Problem1 {
@@ -71,7 +72,7 @@ class Problem1 {
         return total;
     }
 
-    public static ArrayList<Integer> returnScoresToList(int score1, int score2, int score3, int score4) {
+    public static int determineScores(int score1, int score2, int score3, int score4) {
         ArrayList<Integer> arrList = new ArrayList<>();
 
         arrList.add(score1);
@@ -79,7 +80,7 @@ class Problem1 {
         arrList.add(score3);
         arrList.add(score4);
 
-        return arrList;
+        return Collections.max(arrList);
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
